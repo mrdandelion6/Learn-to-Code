2a8650bf94ed3d09b824f6eb70232c6240fa26d9
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-01-13 17:47:09 -0500

    updated gate ignore

--- notes.c
+++ notes.c
@@ -1,7 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-    printf("hello world");
+int main() {
+    double num1, num2;
+    printf("Type two numbers separated by a space: ");
+    scanf("%lf %lf", &num1, &num2);
+    printf("You typed %f and %f\n", num1, num2);
     return 0;
 }
