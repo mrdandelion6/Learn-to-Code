b35d96d27b1e14b45e997e391c30f8ee189bdf32
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-01-14 14:24:03 -0500

    updated git ignore, added a main.c for testing code directly if needed,  and created functions outside of just main();

--- notes.c
+++ notes.c
@@ -1,9 +1,25 @@
-#include <stdio.h>
+// std stands for standard library, io for the input/output, and .h to signify header code
+#include <stdio.h> // for scanf and printf
+#include <stdbool.h> // for using bool types
 
+
+// C source code has an entry point just like Java, which is the main() method.
+// note at the top we have #include statements. this is needed for C for using several (common) methods, types, etc.
 int main() {
+
+    // in C, even for functions, we must declare the function signatures ahead of time
+    int takeInput(); 
+    return takeInput();
+}
+
+int takeInput() {
     double num1, num2;
     printf("Type two numbers separated by a space: ");
     scanf("%lf %lf", &num1, &num2);
     printf("You typed %f and %f\n", num1, num2);
     return 0;
+}
+
+bool test() {
+    return true;
 }
