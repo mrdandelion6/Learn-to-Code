e44eb4914ce254e2b35c5eaf9f2dcd7ac255646e
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-01-15 02:20:14 -0500

    arrays, array iteration, array memory

--- notes.c
+++ notes.c
@@ -4,7 +4,8 @@
 #include "functions.h" // for sharing functions
 #include <stdio.h> // for scanf and printf
 #include <stdbool.h> // for using bool types
-
+#define DAYS 365 
+// use define to define a constant global value!
 
 // C source code has an entry point just like Java, which is the main() method.
 // note at the top we have #include statements. this is needed for C for using several (common) methods, types, etc.
@@ -13,8 +14,10 @@ int main() {
     // in C, even for functions, we must declare the function signatures ahead of time
     int takeInput();
     int average_temp();
+    int iteratingThroughArrays();
+    int loopIterationQuestion();
+    loopIterationQuestion();
 
-    averageTemp();
     return 0;
 }
 
@@ -63,11 +66,42 @@ int averageTemp() {
     daytime_high[3] = 19.1;
 
     int index = 1;
-    printf("On day %d, the high was %f.\n", index, daytime_high[index]);    
+    printf("On day %d, the high was %f.\n", index, daytime_high[index]);  
+
+    // array memory in C, line by line memory status
+    int A[3]; // an array of 3 elements in declared in the stack, initial values are junk; whatever was previously in those memory addresses
+    A[0] = 13; // address values of A[0] is set to value of 13
+    A[1] = 55; // same ..
+    A[2] = 20; // same ..
+
+    // we can accomplish the same thing with less syntax.
+    // initialize the final value at the same time as declaration
+    int B[3] = {13, 55, 20}; 
+    // arrays are contiguous in memory.
+    // this means a space for the entire array is assigned in memory as soon as the array is declared
+    // the following elements of the array will have their values stored in memory side by side, no gaps in memory
+    // by consequence, arrays cannot change in size
+
+    // for element addresses, the adress of array[0] is same as the address of array
+    // if it is an integer array, each element is dedicated 4 bytes or 32 bits
+    // address of array[n] = address of array[n-1] + 4 in this case, as each element will take 4 bytes
+    // addresses are often displayed in hex
+
+    // trying to access an index out of bounds in C doesn't cause an error, but it will lead to unexpected behaviour (value is typically 0, but it could be anything)
+    // C does not care if u try to access memory out of an array's bounds
+
+    // we could do something like:
+    B[5] = 999;
+    // and the compiler won't care. but this is very dangerous as we may be overriding values in memory that another variable is using.
+    // leads to errors that are hard to detect.
+
+    // doing something like
+    // int x = A[9999999]; // can lead to a Segmentation fault when compiling
+
     return 0;
 }
 
-int arrayQuestions() {
+int arrayQuestions1() {
     // self test question 1A:
     // we need a variable to store the top 20 high scores for a video game we are programming. 
     // scores are integer values. declare an array named high_score that holds 20 integers.
@@ -83,6 +117,61 @@ int arrayQuestions() {
     ages[2] = 18;
 
     // self test question 1C:
+    // an airport shuttle is cheaper per person if you have more people in your group. 
+    // in the code fragment below, the array named cost holds the shuttles cost for 
+    // groups of size 0 to 3. the variable group_size is set in code that is not shown. 
+    // use group_size to access the cost array and assign the correct value to total_cost.
+    
+    int group_size;     // value set in hidden code
+    float total_cost;
+    float cost[4];
+    cost[0] = 0;
+    cost[1] = 10.75;
+    cost[2] = 20.50;
+    cost[3] = 30.0;
 
+    total_cost = cost[group_size];
+}
 
+int iteratingThroughArrays() { 
+    // array iteration
+    /// === FOR LOOPS ===
+    float daytime_high[4] = {16.0, 12.8, 14.6, 19.1};
+    float average_temp = 0;
+
+    int i;
+    for (i = 0; i < 4; i++) { // same as java
+        average_temp += daytime_high[i];
+        printf("average %f\n", average_temp);
+    }
+
+    average_temp = average_temp / 4.0;
+
+    return 0;
+}
+
+int loopIterationQuestion() {
+    // write a code fragment to loop over an array <ages> of <count> integers and 
+    // print the age of the second oldest. uou may assume that count is at least 2.
+    int count = 10;
+    // variables count and ages are declared and initialized in hidden code.
+    
+    int ages[10] = {11, 5, 2, 9, 4, 12, 10, 8, 10, 13};
+    int i;
+    int secondOldest = 0;
+    int oldest = 0;
+    for (i = 0; i < count; i++) {
+        if (ages[i] > oldest) {
+            secondOldest = oldest;
+            oldest = ages[i];
+        }
+
+        else if (ages[i] > secondOldest) {
+            secondOldest = ages[i];
+        }
+
+    }
+    printf("%d\n", secondOldest);
+
+    return 0;
 }
