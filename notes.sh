249024bf7cb8b1dbde4df98f2e270d3b430d13e4
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-01-14 19:46:56 -0500

    junction links, finding running processes, commands

--- notes.sh
+++ notes.sh
@@ -1,5 +1,7 @@
 # Shell Scripting Notes
 
+
+# === RUNNING SHELL SCRIPTS ===
 # we can make shell script files which automatically run when executed!
 
 # step 1: create the shell script .sh file, let's call it myscript.sh
@@ -16,11 +18,38 @@
 # for executables added to the system's PATH environment, we do not need ./ as those executables/programs are "global"
 # an example is how we type in gcc without ./
 
-# SSH
+
+# === SSH ===
 # to ssh into a remote location use the following command:
 # ssh <username>@<remote_host>
 # for example, <utorid>@dhXXXXpcXX.utm.utoronto.ca
 # type exit to exit the ssh
+
+
+# === ASIDE ===
+# other useful command line / terminal / bash / powershell things that may be specific to windows
+
+# FIND ANY PROCESSES RUNNING FROM SPECIFIC DIRECTORY (windows):
+# run powershell as admin, and enter: Get-Process | Where-Object { $_.Path -like "C:\path\to\dir*" }
+# replace C with the proper drive, D, E, F, etc.
+# power shell will print all processes running from the specificed directory, eg;
+# Handles  NPM(K)    PM(K)      WS(K)     CPU(s)     Id  SI ProcessName
+# -------  ------    -----      -----     ------     --  -- -----------
+#     151       8     1680       5232       0.05  21128   0 XSpltVidSvc
+
+# JUNCTION LINKS (windows):
+# open cmd as admin and navigate to the directory where you want to create the links
+# enter: mklink /J "link-name" "path\to\actual\stuff"
+# this will create a symbolic junction link "link-name" in the present working directory which points to "path\to\actual\stuff".
+# note that you can only point to directories, not other files.
+# junction links are very useful for keeping synced folders, allowing access to the same content in two different places
+# whereas shortcuts are merely a link that jumps you to the source.
+
+
+# === COMMANDS ===
+# cd <path>: change directory to path, can use .. in place of <path> to jump into current directory's parent directory
+# ls: list present files in current directory
+# gcc -o main main.c: 
 
 # DECLARING VARIABLES
 x="Hello World!"
