e6a81580538e377d3d2a1912c269353c4e88a842
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-05-22 01:11:11 -0400

    added notes for ruby

---
+++ ruby/notes.rb
@@ -0,0 +1,21 @@
+def contents()
+    intro()
+    variables()
+
+end
+
+def intro()
+    # ruby is a dynamic, object-oriented programming language with a focus on simplicity and productivity
+    # the syntax is similiar to python but all functions are method calls
+    # everything is an object in ruby
+    # ruby also has blocks which are similar to anonymous lambda functions in python
+
+end
+
+def variables()
+    #
+    var x = 1
+    asfdsfd
+    d
+
+end
