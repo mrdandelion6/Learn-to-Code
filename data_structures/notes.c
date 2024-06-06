66b3a86a9d487829cc56d2bda60ad47bb5a00a64
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-06-06 19:48:09 -0400

    added notes for data structures

---
+++ data_structures/notes.c
@@ -0,0 +1,46 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    // welcome to my notes on data structures.
+    int main_topics();
+    int ADTs();
+
+    // run
+    ADTs();
+
+    return 0;
+}
+
+int main_topics() {
+    // the main topics ill go over in these notes:
+
+    // 1. balance trees
+    // 2. hash tables
+    // 3. graphs
+    // 4. disjoint sets
+    // 5. priority queues
+    // 6. amortized analysis
+
+    return 0;
+}
+
+int ADTs() {
+    // ADT: Abstract Data Type
+    // an ADT defines:
+        // a set of objects
+        // a set of operations that can be performed on said objects
+        // possibly also their time and space costs (memory and performance)
+        // DOESNT DEFINE HOW THEY ARE IMPLEMENTED!!
+
+    // the implementations of ADTs are called data structures
+
+    // some things that make ADTs great
+        // they just provide a specification; a framework for how things work
+        // moduality: you can change the implementation without changing the interface
+        // reusability: you can use the same ADT in different programs
+
+    return 0;
+}
+
