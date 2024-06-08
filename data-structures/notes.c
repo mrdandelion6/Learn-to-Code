b86b2a67fb4e854958cddb52fe7bee50f7782b86
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-06-07 21:54:00 -0400

    aded notes on data structures

--- data-structures/notes.c
+++ data-structures/notes.c
@@ -1,5 +1,3 @@
-#define _GNU_SOURCE
-
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
@@ -8,10 +6,9 @@ int main(int argc, char* argv[]) {
     // welcome to my notes on data structures.
     int main_topics();
     int ADTs();
-    int algorithm_time();
 
     // run
-    algorithm_time();
+    ADTs();
 
     return 0;
 }
@@ -47,34 +44,3 @@ int ADTs() {
     return 0;
 }
 
-void print_int_arr(int* arr, int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-}
-
-int algorithm_time() {
-
-    // lets take a look at the following algo:
-    #define N 10
-    int A[N] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-
-    int i = 1;
-    while ( i < N ) {
-        int v = A[i];
-        int j = i;
-        while ( j > 0 && A[j-1] > v ) {
-            A[j] = A[j-1];
-            j--;
-        }
-        A[j] = v;
-        i++;
-    }
-    // sorts the array to ascending
-
-    print_int_arr(A, N);
-
-    return 0;
-}
