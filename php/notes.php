5d9ec4635f92da8d6566dddbc3a647cb17ecee39
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-07-20 13:56:15 -0400

    added notes for HTTP and PHP

--- php/notes.php
+++ php/notes.php
@@ -1,9 +1,14 @@
 <?php
 
+// RUN:
+how_php_works();
+
 function contents() {
     // list of topics
     intro_to_php();
     how_php_works();
+    php_tags();
+    running_php();
 }
 
 function intro_to_php() {
@@ -18,7 +23,12 @@ function intro_to_php() {
     // this allows for dynamic content to be generated on the server before being sent to the client
         // for example, interacting with a database to retrieve data
 
+    // YOU CAN HAVE HTML IN PHP FILES
+    // anything outside the php tags is treated as html
+    // see php_tags() for an more
+
     // PHP IS EMEDDABLE IN HTML
+    // this is not inside HTML files, but rather inside HTML tags within PHP files
     // this can be done using the <?php ?\> tags(no \ after the ?)
 
     // for example:
@@ -51,5 +61,55 @@ function how_php_works() {
     // 1. REQUEST
         // a user requests a page via a web browser
     // 2. SERVER PROCESSING
-        // the php preprocessor executes the php code, this may involve interacting with a database, processing user input, or performing other operations
+        // the web server receives the request and finds the php script
+    // 3. PHP EXECUTION
+        // the php preprocessor executes the php code. this may invovlve retrieving information from a database, processing uer input, etc.
+    // 4. RESPONSE
+        // the php script sends html outout back to the browser after processing
+    // 5. DISPLAY
+        // the user's browser displays the resulting html
+    
+    // example script:
+    echo "Hello, World!\n\r";
+}
+
+function php_tags() {
+    // php code is enclosed in <?php ?\> tags
+    // you can see one at the beginning of this file
+    // these tags tell the php interpreter where the php code begins and ends
+    
+    // you can have multiple php tags in a php file.
+    // anything outside of the php tags is treated as html
+    // for example, lets end our php code and write some html
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Tags Example</title>
+</head>
+<body>
+    <h1><?php echo "Hello, World!"; ?></h1>
+    <p>Today's date is <?php echo date('F j, Y'); ?>.</p>
+</body>
+</html>
+
+<?php
+
+function running_php() {
+    // there are several ways to run php code:
+    // 1. web server
+        // the most common way to run php code is on a web server
+        // the web server processes the php code and sends the output to the client's browser
+    // 2. command line
+        // you can run php code from the command line using the php interpreter
+        // this is useful for testing and debugging php code
+    
+    // lets first look at running php in the command line.
+    // simply create a php file and run it with the php command:
+    // php myscript.php
+    // of course, you need to have php interpreter on your system to do this
+    
+    // recall that PHP outputs html, so you can run php code in the command line and see any output html in the terminal
+
 }
