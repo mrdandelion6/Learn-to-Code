81acb4a17ec6f67235464929584738393aa60a56
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-11-10 04:50:45 -0500

    added example project for cmake notes

---
+++ languages/c++/example-project/lib/math/math.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "math.h"
+
+float square_root(int n, int e) {
+    // Return a float o such that |o*o - n| < e.
+    // That is, find the square root of n within some error margin e. 
+    
+
+}
