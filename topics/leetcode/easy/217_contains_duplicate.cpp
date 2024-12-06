68ca01749973b650d5581b25b833c8b9601cb235
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-12-06 05:55:00 -0500

    finished leetcode 217: contains duplicate in c++

--- topics/leetcode/easy/217_contains_duplicate.cpp
+++ topics/leetcode/easy/217_contains_duplicate.cpp
@@ -1,12 +1,37 @@
 #include <vector>
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, bool> map;
-    }
+    public:
+        bool containsDuplicate(vector<int>& nums) {
+            unordered_set<int> seen; // we use a simple set for this problem
+            for (int num : nums) {
+                if (seen.count(num))
+                    return true;
+                seen.insert(num);
+            }
+            return false;
+        }
+
+        bool usingSecond(vector<int>& nums) {
+            // in this approach, we don't use .count() to see if the set contains it anymore
+            // instead we insert it always and see if the insertion worked or failed
+
+            // INSERT() LOGIC FFOR SETS:
+            // if the insertion failed that means the element was already there
+            // note that insert() returns a pair<iterator, bool>
+                // the iterator (first) points to the element in the set
+                // the bool (second) tells us if the element was added
+            // so insert().second gives us whether the insertion was successful or note. 
+
+            unordered_set<int> seen;
+            for (int num : nums) {
+                if (!seen.insert(num).second) // we get false when the insertion fails, i.e, when a dupe exists!
+                    return true;
+            }
+            return false;
+        }
 };
