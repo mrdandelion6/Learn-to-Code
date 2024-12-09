c0c355e74035a6a4ab8fab28bdedae3fa27993fb
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-12-08 23:43:21 -0500

    created rust notes and list of topicsA

---
+++ languages/rust/notes.rs
@@ -0,0 +1,81 @@
+
+
+// WELCOME TO MY RUST NOTES
+// hi, welcome to my rust notes.
+// in these notes you will learn how to write rust code.
+// we will start from the basics and work our way to more advanced topics.
+// these notes assume you have a decent grasp over programming in general and are familiar with other languages already.
+// it will aid you greatly if you are familiar with C/C++, but do not worry if you aren't.
+// if you are fairly new to programming, you will likely want to use these notes in addition to other resources to fill in any gaps.
+
+fn main() {
+    // run
+}
+
+fn topics() {
+
+    // INTRODUCTION
+    what_is_rust();
+    compiling_rust();
+
+    // BASIC SYNTAX
+    variables();
+    types();
+    functions();
+    control_flows();
+
+    // MODULES AND PROJECT STRUCTURE
+    module_system();
+    visibility_rules();
+    packages_and_crates();
+    external_dependencies();
+
+    // OWNERSHIP AND BORROWING
+    ownership_rules();
+    move_semantics();
+    references_and_borrowing();
+    borrow_checker();
+
+    // COMPOUND TYPES
+    structs_and_methods();
+    enums();
+    pattern_matching();
+    tuples();
+    arrays();
+
+    // ERROR HANDLING
+    result_and_option();
+    question_mark_operator();
+    custom_error_types();
+    propagating_errors();
+
+    // TRAITS AND GENERICS
+    traits();
+    generic_types();
+    generic_functions();
+    trait_bounds();
+    associated_types();
+    default_implementations();
+
+    // MEMORY MANAGEMENT
+    stack();
+    heap();
+    smart_pointers();
+    interior_mutability();
+    drop_trait();
+
+    // CONCURRENCY
+    threads();
+    message_passing();
+    shared_state();
+    send_and_sync();
+    atomic_types();
+
+    // ADVANCED TOPICS
+    unsafe_rust();
+    ffi_bindings();
+    C_bindings();
+    closures();
+    zero_cost_abstraction();
+    macros();
+}
