d1eacafcb62d83c99e8a7978131e0f1b3fa9156a
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-12-11 01:09:06 -0500

    edit notes and add helpers.sql

--- languages/sql/notes.sql
+++ languages/sql/notes.sql
@@ -34,6 +34,7 @@ BEGIN
     SQL stands for structured query language. it is a language used to communicate with databases. 
     query means to ask a question. so SQL is used to ask questions to a database.
     SQL is used to perform tasks such as updating data on a database, or retrieving data from a database.
+    SQL is an interpreted language like python or bash. it is interpreted by the database in use.
     */
 
     /*
@@ -107,10 +108,10 @@ BEGIN
     first run the command: sudo mysql
 
     now, create a new user matching your ubuntu username and configure it to use auth_socket plugin.
-    to do this, run the following commands while in mysql (after u ran sudo mysql)
-    CREATE USER 'your_ubuntu_username'@'localhost' IDENTIFIED WITH auth_socket;
-    GRANT ALL PRIVILEGES ON *.* TO 'your_ubuntu_username'@'localhost';
-    FLUSH PRIVILEGES;
+    to do this, run the following commands while in mysql (after u ran sudo mysql):
+        CREATE USER 'your_ubuntu_username'@'localhost' IDENTIFIED WITH auth_socket;
+        GRANT ALL PRIVILEGES ON *.* TO 'your_ubuntu_username'@'localhost';
+        FLUSH PRIVILEGES;
 
     now exit mysql by running \q
     open a new terminal (same user) and run mysql without sudo: mysql
@@ -190,7 +191,7 @@ BEGIN
 
     /*  
     DUPLICATE PROCEDURES
-    whenever we modify or add some procedures, we will need to drop them and readd them.
+    whenever we modify or add some procedures, we will need to drop them and readd them (because they persist).
     we cannot create a procedure with name X if one with name X already exists.
     that is why we have the command: DROP PROCEDURE IF EXISTS procedure_name; above every procedure creation. 
     */
@@ -201,7 +202,6 @@ BEGIN
         1. run: mysql < notes.sql
         2. run: mysql sql_notes
         3. run: CALL procedure_name;
-
     */
 END //
 DELIMITER ;
@@ -210,29 +210,29 @@ DROP PROCEDURE IF EXISTS databases_and_relations;
 DELIMITER //
 CREATE PROCEDURE databases_and_relations()
 BEGIN
-        /*
-        relational databases are databases that store data in tables. each table is made up of rows and columns.
-        each row represents a record, and each column represents a field. a record is a single instance of data, and a field is a category of data.
-        if you want to store different types of data, you would add that in different table. for example, if your current table stores user data, and you want to store product data, you would create a new table for products.
-        */
+    /*
+    relational databases are databases that store data in tables. each table is made up of rows and columns.
+    each row represents a record, and each column represents a field. a record is a single instance of data, and a field is a category of data.
+    if you want to store different types of data, you would add that in different table. for example, if your current table stores user data, and you want to store product data, you would create a new table for products.
+    */
 
-        /*
-        COLUMNS
-        columns store a category of data. for example "name" might store the name of users
-        */
+    /*
+    COLUMNS
+    columns store a category of data. for example "name" might store the name of users
+    */
 
-        /*
-        ROWS
-        rows store an instance of data. each row corresponds to a single "datapoint", for example a single user
-        just some simple ideas to understand
-        */
+    /*
+    ROWS
+    rows store an instance of data. each row corresponds to a single "datapoint", for example a single user
+    just some simple ideas to understand
+    */
 
-        /*
-        PRIMARY KEYS & FOREIGN KEYS
-        each row of a table has its own unique identifier, called a PRIMARY KEY. this is used to identify the row within the table.
-        the tables can be related to each other, hence the name "relational database".
-        for example, the primary keys of one table can be used as some values in another table. this is called a FOREIGN KEY. see example:
-        */
+    /*
+    PRIMARY KEYS & FOREIGN KEYS
+    each row of a table has its own unique identifier, called a PRIMARY KEY. this is used to identify the row within the table.
+    the tables can be related to each other, hence the name "relational database".
+    for example, the primary keys of one table can be used as some values in another table. this is called a FOREIGN KEY. see example:
+    */
 
    -- TEAMS TABLE
     CREATE TABLE teams (
@@ -247,22 +247,28 @@ BEGIN
         age INT,
         team_id INT
     );
-    -- note that CREATE TABLE is a SQL command used to create a new table. the table name is "users", and it has 4 columns: id, name, age, and team_id.
+    /*
+    note that CREATE TABLE is a SQL command used to create a new table. the table name is "users", and it has 4 columns: id, name, age, and team_id.
+    */
 
-    -- we see that the "users" table has a column called "team_id". this column is a foreign key that references the "id" column of the "teams" table.
-    -- this means that the users table is related to the teams table. 
-    -- this might seem very simple, but there is a good reason for why we choose the relational database model. 
-    -- below we motivate some reasons for why we might want to use a relational database.
+    /*
+    we see that the "users" table has a column called "team_id". this column is a foreign key that references the "id" column of the "teams" table.
+    this means that the users table is related to the teams table. 
+    this might seem very simple, but there is a good reason for why we choose the relational database model. 
+    below we motivate some reasons for why we might want to use a relational database.
+    */
 
-    -- WHY USE A RELATIONAL DATABASE ?
-    -- 1. DATA INTEGRITY: relational databases enforce data integrity. this means that the data stored in the database is accurate and consistent.
-        -- they do this by using primary keys to uniquely identify rows, and foreign keys to relate rows in different tables.
-    -- 2. DATA CONSISTENCY: relational databases ensure that data is consistent across tables. for example, if you delete a row from one table, you can also delete related rows from other tables.
-    -- 3. SCALABILITY: you essentially "normalize" your data by storing it in multiple tables. this makes it easier to scale your database as your data grows.
-        -- NORMALIZATION just refers to splitting your data into multiple tables
-        -- an example is, suppose for the above users table, we had a column called "team_name" instead of "team_id". this would be redundant, because the team name is already stored in the teams table.
-        -- and you might say, "well, why not just store the team name in the users table and get rid of the teams table?" 
-        -- in our case we could do that but suppose we had a column called "team_location" as well, or just a lot of columns related to the team. it is better design to encapsulate all the team data in a separate table and reference it with a foreign key.
+    /*
+    WHY USE A RELATIONAL DATABASE ?
+    1. DATA INTEGRITY: relational databases enforce data integrity. this means that the data stored in the database is accurate and consistent.
+        they do this by using primary keys to uniquely identify rows, and foreign keys to relate rows in different tables.
+    2. DATA CONSISTENCY: relational databases ensure that data is consistent across tables. for example, if you delete a row from one table, you can also delete related rows from other tables.
+    3. SCALABILITY: you essentially "normalize" your data by storing it in multiple tables. this makes it easier to scale your database as your data grows.
+        NORMALIZATION just refers to splitting your data into multiple tables.
+        an example is, suppose for the above users table, we had a column called "team_name" instead of "team_id". this would be redundant, because the team name is already stored in the teams table.
+        and you might say, "well, why not just store the team name in the users table and get rid of the teams table?" 
+        in our case we could do that but suppose we had a column called "team_location" as well, or just a lot of columns related to the team. it is better design to encapsulate all the team data in a separate table and reference it with a foreign key.
+    */
 END //
 DELIMITER ;
 
