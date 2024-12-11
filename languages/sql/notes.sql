36e5c1905700a4cfcec32b0e7174eb53d03ff06e
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-12-10 22:50:57 -0500

    added lots of sql notes

--- languages/sql/notes.sql
+++ languages/sql/notes.sql
@@ -1,61 +1,211 @@
 -- welcome to my SQL notes
+-- in these notes we will start from the basics of SQL and work our way up
+-- below is a list of topics we will cover
 
--- what is SQL ?
--- SQL stands for structured query language. it is a language used to communicate with databases. 
--- query means to ask a question. so SQL is used to ask questions to a database.
--- SQL is used to perform tasks such as updating data on a database, or retrieving data from a database.
+-- --------------------------------------------------------------------------------------------------------------------
+/*
+TOPICS:
 
--- DATABASES
--- databases are used to store data. some popular databases are MySQL, PostgreSQL, SQLite, Oracle, and MongoDB.
--- the way data is stored can vary depending on the database. some databases store data in tables, while others store data in json documents.
+INTRO and SETUP:
+    - what is SQL ?
+    - set up MySQL
+    - how to use these notes
+        - functions & procedures
+    - delimiter keyword
 
--- RELATIONAL DATABASES i.e, TABLES
--- sql is most commonly used with relational databases. relational databases store data in tables like with pandas dataframes or excel spreadsheets.
--- relational databases are databases that store data in tables. each table is made up of rows and columns.
--- each row represents a record, and each column represents a field. a record is a single instance of data, and a field is a category of data.
--- if you want to store differenT types of data, you would add that in different table. for example, if your current table stores user data, and you want to store product data, you would create a new table for products.
+USING SQL:
+    - running SQL servers
+    - databases & relational databases
+    - how is SQL used ?
+*/
+-- --------------------------------------------------------------------------------------------------------------------
 
--- COLUMNS
--- columns store a category of data. for example "name" might store the name of users
+-- RUN:
+CALL DatabasesAndRelations();
 
--- ROWS
--- rows store an instance of data. each row corresponds to a single "datapoint", for example a single user
--- just some simple ideas to understand
+DELIMITER //
+CREATE PROCEDURE WhatIsSql()
+BEGIN
+    /*
+    SQL stands for structured query language. it is a language used to communicate with databases. 
+    query means to ask a question. so SQL is used to ask questions to a database.
+    SQL is used to perform tasks such as updating data on a database, or retrieving data from a database.
+    */
 
--- PRIMARY KEYS & FOREIGN KEYS
--- each row of a table has its own unique identifier, called a PRIMARY KEY. this is used to identify the row within the table.
--- the tables can be related to each other, hence the name "relational database".
--- for example, the primary keys of one table can be used as some values in another table. this is called a FOREIGN KEY. see example:
+    /*
+    DATABASES
+    databases, also known as database management systems (DBMS), are software systems designed to store, manage, and organize data. 
+    some popular databases are MySQL, PostgreSQL, SQLite, Oracle, and MongoDB.
+    the way data is stored can vary depending on the database. some databases store data in tables, while others store data in json documents.
+    you can think databases as sophisticated digital filing systems. 
+    in order to query with these databases, we need some sort of 'query language support', like SQL.
+    */
+END //
+DELIMITER ;
 
--- USERS TABLE
-CREATE TABLE users (
-    id INT PRIMARY KEY,
-    name VARCHAR(50),
-    age INT,
-    team_id INT,
-);
--- note that CREATE TABLE is a SQL command used to create a new table. the table name is "users", and it has 4 columns: id, name, age, and team_id.
+DELIMITER //
+CREATE PROCEDURE SetUpMySQL()
+BEGIN
+    /* 
+    SETUP
+    in these notes, we will be using the MySQL database.
+    these notes will assume you are working on a linux environment like ubuntu.
+    if you are on windows, then it may be harder to follow along, just use GPT or claude to figure out windows configuration.
 
--- TEAMS TABLE
-CREATE TABLE teams (
-    id INT PRIMARY KEY,
-    name VARCHAR(50),
-);
+    DOWNLOAD MYSQL
+    run the following commands to install mysql on your machine (you will need sudo):
+    sudo apt update
+    sudo apt install mysql-server
+    sudo mysql_secure_installation
 
--- we see that the "users" table has a column called "team_id". this column is a foreign key that references the "id" column of the "teams" table.
--- this means that the users table is related to the teams table. 
--- this might seem very simple, but there is a good reason for why we choose the relational database model. 
--- below we motivate some reasons for why we might want to use a relational database.
+    after running mysql_secure_installation, select the following options:
 
--- WHY USE A RELATIONAL DATABASE ?
--- 1. DATA INTEGRITY: relational databases enforce data integrity. this means that the data stored in the database is accurate and consistent.
-    -- they do this by using primary keys to uniquely identify rows, and foreign keys to relate rows in different tables.
--- 2. DATA CONSISTENCY: relational databases ensure that data is consistent across tables. for example, if you delete a row from one table, you can also delete related rows from other tables.
--- 3. SCALABILITY: you essentially "normalize" your data by storing it in multiple tables. this makes it easier to scale your database as your data grows.
-    -- NORMALIZATION just refers to splitting your data into multiple tables
-    -- an example is, suppose for the above users table, we had a column called "team_name" instead of "team_id". this would be redundant, because the team name is already stored in the teams table.
-    -- and you might say, "well, why not just store the team name in the users table and get rid of the teams table?" 
-    -- in our case we could do that but suppose we had a column called "team_location" as well, or just a lot of columns related to the team. it is better design to encapsulate all the team data in a separate table and reference it with a foreign key.
+    ... Would you like to setup VALIDATE PASSWORD component?
+    $ y
+
+    ... Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG: 1
+    $ 1
+
+    ... Remove anonymous users?
+    $ y
+
+    ... Disallow root login remotely?
+    $ y
+
+    ... Remove test database and access to it?
+    $ y
+
+    and to confirm installation, run:
+    sudo systemctl status mysql
+
+    you should see something like:
+    ● mysql.service - MySQL Community Server
+        Loaded: loaded (/lib/systemd/system/mysql.service; enabled; vendor preset: enabled)
+        Active: active (running) since Tue 2024-12-10 22:00:05 EST; 2min 31s ago
+        Process: 241786 ExecStartPre=/usr/share/mysql/mysql-systemd-start pre (code=exited, status=0/SUCCESS)
+    Main PID: 241794 (mysqld)
+        Status: "Server is operational"
+        Tasks: 39 (limit: 16702)
+        Memory: 360.5M
+        CGroup: /system.slice/mysql.service
+                └─241794 /usr/sbin/mysqld
+
+    Dec 10 22:00:04 Acer-DK systemd[1]: Starting MySQL Community Server...
+    Dec 10 22:00:05 Acer-DK systemd[1]: Started MySQL Community Server.
+     */
+
+    /* 
+    UNIX SOCKET AUTHENTICATION
+    you need to now setup local authentication that uses your system user credentials.
+    this is so we don't have to use sudo each time we want to run mysql.
+
+    first run the command: sudo mysql
+
+    now, create a new user matching your ubuntu username and configure it to use auth_socket plugin.
+    to do this, run the following commands while in mysql (after u ran sudo mysql)
+    CREATE USER 'your_ubuntu_username'@'localhost' IDENTIFIED WITH auth_socket;
+    GRANT ALL PRIVILEGES ON *.* TO 'your_ubuntu_username'@'localhost';
+    FLUSH PRIVILEGES;
+
+    now exit mysql by running \q
+    open a new terminal (same user) and run mysql without sudo: mysql
+    this should now work.
+    */
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE HowToUseTheseNotes()
+BEGIN
+    /* 
+    RUNNING CODE FROM NOTES
+    before proceeding with understanding SQL itself, we must first see how we can use these notes.
+    specifically, how we can run the demonstrations and examples i wrote inside them.
+
+    as you can see, each individual section of these notes is sectioned off into it's own "procedure".
+    SQL has both 'functions' and 'stored procedures'.
+    just like functions, procedures are a set of same instructions which can be called again, foo(1, 2)
+    however, SQL functions and SQL procedures have some differences!
+    */
+    
+    /*  
+    STORED PROCEDURES vs FUNCTIONS:
+    stored procedures are pre-written sets of SQL statements that can be saved and reused later, like a recipe you can follow multiple times.
+    
+    STORED PROCEDURES:
+        - can perform multiple operations like INSERT, UPDATE, DELETE, and SELECT
+        - can modify the data in the database
+        - don't need to return a value (though they can)
+        - can have input and output parameters
+        - are typically used for business logic and complex operations
+
+    FUNCTIONS:
+        - must return a value
+        - can't modify data in the database, they are read only
+        - can only contain SELECT statements
+        - are typically used for calculations or data retrieval
+    */
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DatabasesAndRelations()
+BEGIN
+        /*
+        relational databases are databases that store data in tables. each table is made up of rows and columns.
+        each row represents a record, and each column represents a field. a record is a single instance of data, and a field is a category of data.
+        if you want to store different types of data, you would add that in different table. for example, if your current table stores user data, and you want to store product data, you would create a new table for products.
+        */
+
+        /*
+        COLUMNS
+        columns store a category of data. for example "name" might store the name of users
+        */
+
+        /*
+        ROWS
+        rows store an instance of data. each row corresponds to a single "datapoint", for example a single user
+        just some simple ideas to understand
+        */
+
+        /*
+        PRIMARY KEYS & FOREIGN KEYS
+        each row of a table has its own unique identifier, called a PRIMARY KEY. this is used to identify the row within the table.
+        the tables can be related to each other, hence the name "relational database".
+        for example, the primary keys of one table can be used as some values in another table. this is called a FOREIGN KEY. see example:
+        */
+
+   -- TEAMS TABLE
+    CREATE TABLE teams (
+        id INT PRIMARY KEY,
+        name VARCHAR(50)
+    );
+
+    -- USERS TABLE
+    CREATE TABLE users (
+        id INT PRIMARY KEY,
+        name VARCHAR(50),
+        age INT,
+        team_id INT
+    );
+    -- note that CREATE TABLE is a SQL command used to create a new table. the table name is "users", and it has 4 columns: id, name, age, and team_id.
+
+    -- we see that the "users" table has a column called "team_id". this column is a foreign key that references the "id" column of the "teams" table.
+    -- this means that the users table is related to the teams table. 
+    -- this might seem very simple, but there is a good reason for why we choose the relational database model. 
+    -- below we motivate some reasons for why we might want to use a relational database.
+
+    -- WHY USE A RELATIONAL DATABASE ?
+    -- 1. DATA INTEGRITY: relational databases enforce data integrity. this means that the data stored in the database is accurate and consistent.
+        -- they do this by using primary keys to uniquely identify rows, and foreign keys to relate rows in different tables.
+    -- 2. DATA CONSISTENCY: relational databases ensure that data is consistent across tables. for example, if you delete a row from one table, you can also delete related rows from other tables.
+    -- 3. SCALABILITY: you essentially "normalize" your data by storing it in multiple tables. this makes it easier to scale your database as your data grows.
+        -- NORMALIZATION just refers to splitting your data into multiple tables
+        -- an example is, suppose for the above users table, we had a column called "team_name" instead of "team_id". this would be redundant, because the team name is already stored in the teams table.
+        -- and you might say, "well, why not just store the team name in the users table and get rid of the teams table?" 
+        -- in our case we could do that but suppose we had a column called "team_location" as well, or just a lot of columns related to the team. it is better design to encapsulate all the team data in a separate table and reference it with a foreign key.
+END //
+DELIMITER ;
 
 -- so SQL is not ony used to create, read, update, and delete data (CRUD), but it also gives data a structure.
 
