aae1ed0b46a9cfa0958c54283646340cfc653c0c
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-12-23 00:53:54 -0500

    add lua notes

--- languages/lua/notes.lua
+++ languages/lua/notes.lua
@@ -4,7 +4,22 @@ function Contents()
 	Basic_syntax()
 end
 
-function What_is_lua() end
+function What_is_lua()
+	-- lua is is an interpreted, lightweight, high-level programming language.
+	-- it was created two main principles in mind:
+	-- 1. simplicity
+	-- 2. embeddability
+
+	-- lua is primarily used for the following things:
+	-- 1. game dev
+	--	lua is extremely popular for game dev. it is used for scripting in WoW, roblox, and gmod. game devs often embed lua to allow modding and to handle game logic without recompiling the main engine.
+	-- 2. embedded systems
+	--	due to its small and efficient design, lua is frequently used in embedded systems and IoT devices where resources are limited
+	-- 3. configuration
+	--	many applications use lua as a configuration language because of its clear syntax and ability to handle complex configurations in an elegant way. for example, neovim.
+	-- 4. application scripting
+	--	software like adobe lightroom and neovim use lua for extensibility and plugin development.
+end
 
 function Lua_for_vim() end
 
